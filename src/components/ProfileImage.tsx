93be4951cee4d88dde9a718990c359121e577be6
Hendra Wong <hendrakwong@gmail.com>
2025-05-22 11:18:07 -0700

    Include Profile Photo

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import profilePhoto from '../assets/images/profile-photo.jpeg';
 
 const ProfileImage: React.FC = () => {
   return (
     <div className="relative w-48 h-48 md:w-64 md:h-64 rounded-full overflow-hidden border-4 border-white dark:border-gray-800 shadow-lg">
-      <div className="absolute inset-0 bg-gradient-to-br from-blue-100 to-blue-200 dark:from-blue-900 dark:to-blue-800 flex items-center justify-center">
-        <span className="text-blue-500 dark:text-blue-300 font-medium text-sm">Profile Photo</span>
-      </div>
-      {/* Replace this div with an actual image tag when you have a photo */}
-      {/* <img src="your-image-url.jpg" alt="Hendra Wong" className="w-full h-full object-cover" /> */}
+      <img 
+        src={profilePhoto} 
+        alt="Hendra Wong" 
+        className="w-full h-full object-cover"
+        loading="lazy"
+      />
     </div>
   );
 };
